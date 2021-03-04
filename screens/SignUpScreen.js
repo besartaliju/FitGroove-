21b0dcfb0dfe1083f6d934fa3409a7e954fc47ca
rduraj96 <42787617+rduraj96@users.noreply.github.com>
2021-03-04 00:25:46 -0500

    Add user to DB

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,7 +1,7 @@
 import React, { useLayoutEffect, useState } from 'react';
 import { Button, Input, Image } from "react-native-elements";
 import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 
 const SignUpScreen = ({ navigation }) => {
     const [name, setName] = useState('');
@@ -15,8 +15,9 @@ const SignUpScreen = ({ navigation }) => {
         })
     }, [navigation])
 
-    const signUp = () => {
-        auth
+    const signUp = async () => {
+        try {
+            await auth
             .createUserWithEmailAndPassword(email,password)
             .then((authUser) => {
                 authUser.user.updateProfile({
@@ -24,8 +25,42 @@ const SignUpScreen = ({ navigation }) => {
                     photoURL: avatar || "https://st4.depositphotos.com/4329009/19956/v/600/depositphotos_199564354-stock-illustration-creative-vector-illustration-default-avatar.jpg"
                 })
             })
+            // .onAuthStateChanged((user) => {
+            //     alert(user.uid);
+            //     if (user) {
+            //         db
+            //             .collection('users')
+            //             .doc(user.uid)
+            //             .add({
+            //                 name: name,
+            //                 details: {
+            //                     age: 0,
+            //                     height: 0,
+            //                     weight: 0
+            //                 }
+            //             })
+            //     }
+            // })
             .catch((err) => alert(err.message))
+            let userID = auth?.currentUser?.uid;
+            await db
+                .collection('users')
+                .doc(userID)
+                .set({
+                    name: name,
+                    details: {
+                        age: 0,
+                        height: 0,
+                        weight: 0
+                    }
+                })
+
+        } catch (err) {
+            alert(err.message) 
+        }
     }
+
+
 
     return (
         <KeyboardAvoidingView behavior="padding" style={styles.container}>
