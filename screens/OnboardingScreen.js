cb65fedb831bf5c93a14fdbbf60e3916253c6bc8
Besart Aliju <alijubesart@gmail.com>
2021-04-07 01:47:48 -0400

    Update OnboardingScreen.js

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -5,17 +5,23 @@ import { auth } from "../firebase";
 import DateTimePicker from '@react-native-community/datetimepicker';
 //import DatePicker from 'react-native-date-picker'
 
-import {View, Text, Button, Image, StyleSheet, TextInput, Input} from 'react-native';
+import {View, Text, Button, Image, StyleSheet, TextInput, Input, TouchableWithoutFeedback, Keyboard} from 'react-native';
 import styled from "styled-components/native";
 import {Feather as Icon} from "@expo/vector-icons";
 import {Picker} from '@react-native-picker/picker';
 
 const OnboardingScreen = ({navigation}) => {
 
-    const [date, setDate] = useState(new Date())
+    const [date, setDate] = useState(new Date());
     const [height, setHeight] = useState();
     const [weight, setWeight] = useState();
     const [goalWeight, setGoalWeight] = useState();
+
+    const DismissKeyboard = ({ children }) => (
+        <TouchableWithoutFeedback 
+            onPress={() => Keyboard.dismiss()}>{children}
+        </TouchableWithoutFeedback>
+    );
 
 
     return(
@@ -25,7 +31,18 @@ const OnboardingScreen = ({navigation}) => {
                 <Subheading> Let us know about you to speed up the result, get fit fast with great results! </Subheading>
             </Main>
 
-            <Text> Birthday </Text>
+            <View
+                    style={{
+                        width: 400,
+                        borderBottomColor: 'black',
+                        borderBottomWidth: 2,
+                        paddingleft: 25,
+                        paddingright: 25,
+                        paddingbottom: 5,
+                    }}
+            />
+
+            <Text style={styles.text}> Birthday </Text>
             <DateTimePicker
                     testID="dateTimePicker"
                     value={date}
@@ -35,52 +52,84 @@ const OnboardingScreen = ({navigation}) => {
                     //onChange={onChange}
             />
 
-            <Text> Height </Text>
-            <TextInput 
-                onChangeText={setHeight}
-                value={height}
-            >  
-            
-            </TextInput>
+                <View
+                    style={{
+                        width: 400,
+                        borderBottomColor: 'black',
+                        borderBottomWidth: 2,
+                        paddingleft: 25,
+                        paddingright: 25,
+                        paddingbottom: 5,
+                        paddingtop: 5,
+                    }}
+                />
 
+                <Text style={styles.text}> Height </Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setHeight}
+                    value={height}
+                >  
+                </TextInput>
 
+                <View
+                    style={{
+                        width: 400,
+                        borderBottomColor: 'black',
+                        borderBottomWidth: 2,
+                        paddingleft: 25,
+                        paddingright: 25,
+                        paddingbottom: 5,
+                        paddingtop: 5,
+                    }}
+                />
 
-            {/* <Picker
-                    selectedValue={height}
-                    onValueChange={(itemValue, itemIndex) =>
-                    setHeight(itemValue)
-                }>
-            
-                <Picker.Item label="5 ft" value="5ft" />
-                <Picker.Item label="6 ft" value="6ft" />
-            </Picker> */}
+                <View>
+                    <Text style={styles.text}> Weight </Text>
+                    <TextInput 
+                    style={styles.input}
+                    value={weight}
+                    onChangeText={setWeight}
+                    placeholder="lbs"
+                    keyboardType="numeric"
+                    > 
+                     
+                    </TextInput>
+                </View>   
 
-            <Text> Weight </Text>
-            <TextInput 
-                style={styles.input}
-                onChangeText={setWeight}
-                value={weight}
-                placeholder="lbs"
-                keyboardType="numeric"
-            >  
-            
-            </TextInput>
+                <View
+                    style={{
+                        width: 400,
+                        borderBottomColor: 'black',
+                        borderBottomWidth: 2,
+                        paddingleft: 25,
+                        paddingright: 25,
+                        paddingbottom: 5,
+                        paddingtop: 5,
+                    }}
+                />
 
-
-            <Text> Goal Weight</Text>
+            <Text style={styles.text}> Goal Weight</Text>
             <TextInput 
                 style={styles.input}
                 onChangeText={setGoalWeight}
                 value={goalWeight}
                 placeholder="lbs"
-                
+                keyboardType="numeric"
             >  
-            </TextInput>
+            </TextInput>     
 
-            {/* <BirthdayText>
-                Birthday     
-            </BirthdayText> */}
-                
+            <View
+                    style={{
+                        width: 400,
+                        borderBottomColor: 'black',
+                        borderBottomWidth: 2,
+                        paddingleft: 25,
+                        paddingright: 25,
+                        paddingbottom: 5,
+                        paddingtop: 5,
+                    }}
+                />
         </Container>
 
     );
@@ -91,21 +140,40 @@ export default OnboardingScreen;
 //const styles = StyleSheet.create({})
 
 const styles = StyleSheet.create({
+
+    row: {
+        flex: 1,
+        flexDirection: "row",
+        marginVertical: 5
+      },
+
+    text: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: '#808080'
+    },
     input: {
-      height: 40,
-      margin: 12,
+      height: 30,
+      width: '66%',
+      margin: 15,
       borderWidth: 1,
+      backgroundColor: '#FFFFF8',
     },
   });
 
-const Container = styled.View`
+  
+
+  //margin: 5px 25px 50px 25px;
+  const Container = styled(Platform.OS === 'ios' ? Kav : View).attrs({
+    behavior: Platform.OS === 'ios' && 'position',
+  })`
     flex: 1;
-    background: #FFFFFF;    
-`;
+    background: #060507;
+  `;
 
  const Main = styled.View`
-     margin: 5px 25px 50px 25px;
-     background: #808080;
+     padding: 5px 25px 50px 25px;
+     background: #060507;
 
  `;
 
@@ -123,7 +191,7 @@ const Heading = styled.Text`
 const Subheading = styled.Text`
     margin: 10px 50px 5px 50px;
     width: 220px;
-    height: px;
+    height: 100px;
     font-size: 18px;
     line-height: 28px;
     textAlign: center;
@@ -142,4 +210,12 @@ const BirthdayText = styled.Text`
     height: 30px; 
     color: #949397;
     margin: 5px 50px 10px 50px;
+    fontSize: 20,
+    fontWeight: "bold",
+    color: '#808080'
 `;
+
+// const Section = styled.View`
+//     flex: 1;
+//     flexDirection: row,
+// `;
